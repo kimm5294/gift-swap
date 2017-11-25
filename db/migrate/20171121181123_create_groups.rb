e3c0220174da3c80b26a0fcedda77f3b6e5cae53
Matt Kim <MattKim@Matts-MacBook-Pro-2.local>
2017-11-25 15:30:32 -0500

    add fields to migration file for groups

--- db/migrate/20171121181123_create_groups.rb
+++ db/migrate/20171121181123_create_groups.rb
@@ -1,6 +1,9 @@
 class CreateGroups < ActiveRecord::Migration[5.1]
   def change
     create_table :groups do |t|
+      t.string :name, null: false
+      t.integer :price_cap, null: false
+      t.datetime :date, null: false
 
       t.timestamps
     end
