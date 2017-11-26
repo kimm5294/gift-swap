7b587ce0dc5b98e32290dbc899efd6526812c7c7
Matt Kim <MattKim@Matts-MacBook-Pro-2.local>
2017-11-25 22:09:11 -0500

    create migration files for item and items_user

--- db/migrate/20171126022720_create_items.rb
+++ db/migrate/20171126022720_create_items.rb
@@ -1,6 +1,10 @@
 class CreateItems < ActiveRecord::Migration[5.1]
   def change
     create_table :items do |t|
+      t.string :name, null: false
+      t.string :url
+      t.string :description
+      t.string :amazon_id
 
       t.timestamps
     end
--- db/migrate/20171126023926_create_items_users.rb
+++ db/migrate/20171126023926_create_items_users.rb
@@ -1,6 +1,8 @@
 class CreateItemsUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :items_users do |t|
+      t.references :user, null: false
+      t.references :item, null: false
 
       t.timestamps
     end
