28670217c82e4c97535eb28e3155081ffbc7d816
Matt Kim <MattKim@Matts-MacBook-Pro-2.local>
2017-11-27 22:49:37 -0500

    add current_user, logged_in and authenticate user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  def authenticate_user
+    if !current_user
+      redirect_to signin_path, notice: "You must be signed in to do that!"
+    end
+  end
+
+  helper_method :current_user, :authenticate_user
 end
