9df9e8e683229d1405c48fa2d7c9547c48b6276f
Matt Kim <MattKim@Matts-MacBook-Pro-2.local>
2017-12-04 15:29:21 -0500

    create new and create routes for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to "/users/#{@user.id}"
+    else
+      @errors = @user.errors.full_messages
+      render 'new'
+    end
+  end
 end
